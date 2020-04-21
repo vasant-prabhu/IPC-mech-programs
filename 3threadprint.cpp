68d10ab7e363b577a5c8aa7c5e0bb8cdc73699e5
vasant <vasant_prabhu@yahoo.co.in>
2020-04-21 14:04:58 +0530

    3 threads print 1 2 3 ,4 5 6 cpp11 thread class

---
+++ 3threadprint.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+#include<thread>
+#include<condition_variable>
+#include<chrono>
+std::mutex mu;
+std::condition_variable cv,cv2,cv3;
+using namespace std;
+bool result1=false;
+bool result2=false;
+bool result3=true;
+
+void func1()
+{
+   int i=1;
+   while(1)
+   {
+      std::unique_lock<std::mutex>lck(mu);
+      cv3.wait(lck,[]{return result3?true:false;});
+      result3=false;
+      cout<<i<<endl;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      i+=3;
+      result1=true;
+      lck.unlock();
+      cv.notify_one();
+      if(i>100)break;
+   }
+}
+
+void func2()
+{
+   int i=2;
+   while(1)
+   {
+      std::unique_lock<std::mutex>lck(mu);
+      cv.wait(lck,[]{return result1?true:false;});
+      result1=false;
+      cout<<i<<endl;
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      i+=3;
+      
+      result2=true;
+      lck.unlock();
+      cv2.notify_one();
+      if(i>100)break;
+   }
+}
+
+void func3()
+{  
+    int i=3;
+    while(1)
+    {  
+      std::unique_lock<std::mutex>lck(mu);
+      cv2.wait(lck,[]{return result2?true:false;});
+      result2=false;
+      cout<<i<<endl;
+      i+=3;
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      result3=true;
+      lck.unlock();
+      cv3.notify_one();
+      if(i>100)break;
+    }
+}
+
+//driver code
+int main()
+{
+std::thread t1(func1);
+std::thread t2(func2);
+std::thread t3(func3);
+
+t1.join();
+t2.join();
+t3.join();
+
+return 0;
+}
