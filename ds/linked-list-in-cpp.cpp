e74fc665549e517b673415c1124ce974c25f4619
vasant <vasant_prabhu@yahoo.co.in>
2020-04-24 20:48:23 +0530

    Added linked list reversal by iterative & recursive method

--- ds/linked-list-in-cpp.cpp
+++ ds/linked-list-in-cpp.cpp
@@ -20,6 +20,11 @@ void insertNode(int val);
 void deleteNode(int val);
 void deleteNodeAtPosition(int position);
 void printList();
+void PrintReverseWrapper();
+void printListInReverseOrder(Node *head);
+void iterative_reversalOfLL();
+void RecursiveReverseLinkedListWrapper();
+void RecursiveReverseLinkedList(Node *head);
 void clear();
 };
 
@@ -96,8 +101,25 @@ void List::printList()
 		std::cout<<temp->data<<" ";
 		temp=temp->next;
 	}
+	std::cout<<std::endl;
 }
 
+
+void List::PrintReverseWrapper()
+{
+	printListInReverseOrder(head);
+}
+
+//recursion used to print linked list in reverse
+void List::printListInReverseOrder(Node *head)
+{
+	if(head!=NULL)
+	{		
+		if(head==nullptr)return;
+		printListInReverseOrder(head->next);
+		std::cout<<head->data<<" ";
+	}	
+}
 
 void List::clear()
 {
@@ -114,6 +136,41 @@ void List::clear()
 		head=temp;
 		temp=temp->next;
 	}
+}
+
+void List::iterative_reversalOfLL()
+{
+	Node *prev=nullptr;
+	Node *current=head;
+	Node *next=nullptr;
+	while(current!=nullptr)
+	{
+		next=current->next; //save the next node,as we will break the connection,going ahead.
+		current->next=prev;
+		//Shift the prev and current pointers ahead
+		prev=current;
+		current=next;	
+	}
+	head=prev;
+}
+
+void List::RecursiveReverseLinkedListWrapper()
+{
+	Node *current = head;
+	RecursiveReverseLinkedList(current);
+}
+
+
+void List::RecursiveReverseLinkedList(Node *current)
+{
+	if(current->next==nullptr)
+	{
+		head=current;
+		return;
+	}	
+	RecursiveReverseLinkedList(current->next);
+	current->next->next=current;
+	current->next=nullptr;
 }
 
 int main()
@@ -139,7 +196,16 @@ obj1.printList();
 std::cout<<"delete 4th element"<<std::endl;
 obj1.deleteNodeAtPosition(4);
 obj1.printList();
+//std::cout<<"Print list in reverse order"<<std::endl;
+//obj1.PrintReverseWrapper();
 //TODO reverse a linked list.
+std::cout<<"Reversal of Linked list by iterative method"<<std::endl;
+obj1.iterative_reversalOfLL();
+obj1.printList();
+
+std::cout<<"Reversal of Linked list by recursive method"<<std::endl;
+obj1.RecursiveReverseLinkedListWrapper();
+obj1.printList();
 //TODO intersection of 2 linked list
 //TODO detection of a loop in linked list-floyd algorithm
 
